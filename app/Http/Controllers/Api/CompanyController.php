2b1c0de875832c26437fb89eb197ed65bf89a134
LuizColombo <colombo74744@hotmail.com>
2025-10-22 12:23:37 -0300

    Limpeza de Código

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -22,11 +22,6 @@ class CompanyController extends Controller
         $this->companyService = $companyService;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request $request)
     {
         $companies = $this->companyService->getCompanies($request->get('filter', ''));
@@ -34,28 +29,16 @@ class CompanyController extends Controller
         return CompanyResource::collection($companies);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreUpdateCompany $request)
     {
         $company = $this->companyService->createNewCompany($request->validated(), $request->image);
 
         CompanyCreated::dispatch($company->email)
-                            ->onQueue('queue_email');
+            ->onQueue('queue_email');
 
         return new CompanyResource($company);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  string  $uuid
-     * @return \Illuminate\Http\Response
-     */
     public function show($uuid)
     {
         $company = $this->companyService->getCompanyByUUID($uuid);
@@ -63,18 +46,11 @@ class CompanyController extends Controller
         $evaluations = $this->evaluationService->getEvaluationsCompany($uuid);
 
         return (new CompanyResource($company))
-                        ->additional([
-                           'evaluations' => json_decode($evaluations)
-                        ]);
+            ->additional([
+                'evaluations' => json_decode($evaluations)
+            ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string  $uuid
-     * @return \Illuminate\Http\Response
-     */
     public function update(StoreUpdateCompany $request, $uuid)
     {
         $this->companyService->updateCompany($uuid, $request->validated(), $request->image);
@@ -84,12 +60,6 @@ class CompanyController extends Controller
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  string  $uuid
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($uuid)
     {
         $this->companyService->deleteCompany($uuid);
